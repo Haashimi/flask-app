d3e3a4dfc542f418a879478354e76de11f14afc3
haashimi <mohameddiini@outlook.com>
2023-08-20 08:27:10 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+from app import app
+
+if __name__ == '__main__':  
+    print("flask application.")
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
